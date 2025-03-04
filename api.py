9c2a3effe457e6005a1bea5103f500d5f67c7f3a
Zeeshan <zeeshan.quraishy@golar.com>
2025-03-04 14:43:28 +0100

    new endpoints added

--- api.py
+++ api.py
@@ -1,16 +1,37 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST", "GET"])
 def ping():
-    data = request.get_json()
-    msg = data.get("msg")
+    data = request.get_json(silent=True) or {} 
+    msg = data.get("msg")  
 
-    if msg =="ping":
+    if msg == "ping":
         return jsonify(response="Pong")
+    if not data:  
+        return jsonify(response="You forgot to say Hi")
     return jsonify(response="We not talking")
+
+@app.route("/tellme", methods=["GET"])
+def get_example():
+    return "Security is overrated"
+
+
+@app.route("/submit", methods=["GET"])
+def get_example_workload():
+    name = request.args.get("name", "Unknown")  
+    return f"Technology sucks - Dr. {name}"
+
+
+@app.route("/postbody", methods=["POST"])
+def post_example_workload():
+    data = request.get_json(silent=True) or {} 
+    name = data.get("who")
+    return f"Technology sucks - Dr. {name}"
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
